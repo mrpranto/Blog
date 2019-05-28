9b3e82f027680786a0051893db250327a1868448
mrpranto <prantoabir1@gmail.com>
2019-05-28 22:06:14 +0600

    User table migration, Roles Table migration and Roles table seed added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->tinyInteger('role_id');
+            $table->string('name',128);
+            $table->string('email',128)->unique();
+            $table->date('email_verified_at')->nullable();
+            $table->string('email_verification_token',128)->nullable();
+            $table->string('password',128 );
             $table->rememberToken();
             $table->timestamps();
         });
