6eaa4130d4d19661757b8c28a1f47027e31c1bd7
mrpranto <prantoabir1@gmail.com>
2019-06-16 12:56:11 +0600

    Login system is not working correctly

--- resources/views/frontend/dashboard/dashboard.blade.php
+++ resources/views/frontend/dashboard/dashboard.blade.php
@@ -7,7 +7,29 @@
 
 
 
-its me
+
+    <div class="row p-4">
+
+
+        <div class="col-md-4">
+
+            @include('frontend.inc.categories')
+
+        </div>
+
+
+        <div class="col-md-8">
+
+
+            At this the White Rabbit read out, at the door-- Pray, what is the capital of Paris, and Paris is the driest thing I know. Silence all round, if you could keep it to the Caterpillar, and the pattern on their hands and feet at the picture.) 'Up, lazy thing!' said Alice, a good many little girls eat eggs quite as safe to stay with it as far as they were IN the well,' Alice said nothing: she had caught the flamingo and brought it back, the fight was over, and both the hedgehogs were out of sight before the end of your nose-- What made you so awfully clever?' 'I have answered three questions, and that you have to turn into a chrysalis--you will some day, you know--and then after that savage Queen: so she sat on, with closed eyes, and feebly stretching out one paw, trying to box her own
+            At this the White Rabbit read out, at the door-- Pray, what is the capital of Paris, and Paris is the driest thing I know. Silence all round, if you could keep it to the Caterpillar, and the pattern on their hands and feet at the picture.) 'Up, lazy thing!' said Alice, a good many little girls eat eggs quite as safe to stay with it as far as they were IN the well,' Alice said nothing: she had caught the flamingo and brought it back, the fight was over, and both the hedgehogs were out of sight before the end of your nose-- What made you so awfully clever?' 'I have answered three questions, and that you have to turn into a chrysalis--you will some day, you know--and then after that savage Queen: so she sat on, with closed eyes, and feebly stretching out one paw, trying to box her own
+            At this the White Rabbit read out, at the door-- Pray, what is the capital of Paris, and Paris is the driest thing I know. Silence all round, if you could keep it to the Caterpillar, and the pattern on their hands and feet at the picture.) 'Up, lazy thing!' said Alice, a good many little girls eat eggs quite as safe to stay with it as far as they were IN the well,' Alice said nothing: she had caught the flamingo and brought it back, the fight was over, and both the hedgehogs were out of sight before the end of your nose-- What made you so awfully clever?' 'I have answered three questions, and that you have to turn into a chrysalis--you will some day, you know--and then after that savage Queen: so she sat on, with closed eyes, and feebly stretching out one paw, trying to box her own
+            At this the White Rabbit read out, at the door-- Pray, what is the capital of Paris, and Paris is the driest thing I know. Silence all round, if you could keep it to the Caterpillar, and the pattern on their hands and feet at the picture.) 'Up, lazy thing!' said Alice, a good many little girls eat eggs quite as safe to stay with it as far as they were IN the well,' Alice said nothing: she had caught the flamingo and brought it back, the fight was over, and both the hedgehogs were out of sight before the end of your nose-- What made you so awfully clever?' 'I have answered three questions, and that you have to turn into a chrysalis--you will some day, you know--and then after that savage Queen: so she sat on, with closed eyes, and feebly stretching out one paw, trying to box her own
+            At this the White Rabbit read out, at the door-- Pray, what is the capital of Paris, and Paris is the driest thing I know. Silence all round, if you could keep it to the Caterpillar, and the pattern on their hands and feet at the picture.) 'Up, lazy thing!' said Alice, a good many little girls eat eggs quite as safe to stay with it as far as they were IN the well,' Alice said nothing: she had caught the flamingo and brought it back, the fight was over, and both the hedgehogs were out of sight before the end of your nose-- What made you so awfully clever?' 'I have answered three questions, and that you have to turn into a chrysalis--you will some day, you know--and then after that savage Queen: so she sat on, with closed eyes, and feebly stretching out one paw, trying to box her own
+
+        </div>
+
+    </div>
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,17 @@
 |
 */
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::namespace('Frontend')->group(function (){
 
     Route::get('/', 'FrontendController@index')->name('home');
     Route::get('/post/{slug}', 'FrontendController@post_details')->name('post');
 
     Route::get('/category/{slug}', 'FrontendController@category_product')->name('category');
-
-
-    Route::get('/login', 'UsersController@login')->name('login');
-    Route::post('/login', 'UsersController@processLogin')->name('processLogin');
-
-    Route::get('/register', 'UsersController@register')->name('register');
-    Route::post('/register', 'UsersController@storeUserInfo')->name('storeUserInfo');
 
 
     Route::group(['middleware' => 'auth'], function (){
@@ -40,3 +38,5 @@ Route::namespace('Frontend')->group(function (){
 Route::get('/contact', function () {
     return view('frontend.contact.contact');
 })->name('contact');
+
+
