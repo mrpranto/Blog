f6f4fb9a8f8046d7dd2553ef19cd4366843f0503
mrpranto <prantoabir1@gmail.com>
2019-06-16 20:32:12 +0600

    something is change

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+//
+//Auth::routes();
+//
+//Route::get('/home', 'HomeController@index')->name('home');
 
 Route::namespace('Frontend')->group(function (){
 
@@ -23,6 +23,11 @@ Route::namespace('Frontend')->group(function (){
 
     Route::get('/category/{slug}', 'FrontendController@category_product')->name('category');
 
+
+    Route::get('/login', 'UsersController@login')->name('login');
+    Route::post('/login', 'UsersController@processLogin')->name('processLogin');
+    Route::get('/register', 'UsersController@register')->name('register');
+    Route::post('/register', 'UsersController@storeUserInfo')->name('storeUserInfo');
 
     Route::group(['middleware' => 'auth'], function (){
 
@@ -40,3 +45,7 @@ Route::get('/contact', function () {
 })->name('contact');
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
